89bffcc3629272c760ca893c23f063f37a247ecc
AbayReushenov <abay.reushenov@gmail.com>
2020-06-14 23:41:35 +0300

    lesson 17 Создать функцию ShowMyDb, writeYourGenres

--- js/lesson18_zadanie.js
+++ js/lesson18_zadanie.js
@@ -15,13 +15,16 @@ P.S. Функции вызывать не обязательно*/
 
 // Код возьмите из предыдущего домашнего задания
 let numberOfFilms;
-function start() {
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let start = () => {
     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) ) {
         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
-}
+};
 start();
 // 
+
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -29,8 +32,7 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-//
-function rememberMyFilms() {
+
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
@@ -43,10 +45,7 @@ for (let i = 0; i < 2; i++) {
         i--;
     }
 }
-}
-rememberMyFilms();
-//
-function detectPersonalLevel() {
+
 if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -56,26 +55,5 @@ if (personalMovieDB.count < 10) {
 } else {
     console.log("Произошла ошибка");
 }
-}
-detectPersonalLevel();
-//
-//console.log(personalMovieDB);
-//
-//функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-// false - выводит в консоль главный объект программы
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
-showMyDB(personalMovieDB.privat);
-//
-/* 3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres */
-function writeYourGenres() {
-  for (let i =0; i <= 2; i++ ) {
-    personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`);
-  }
-}
-writeYourGenres();
+
+console.log(personalMovieDB);
