168eaa323ca256c388a1d466a2c7d77268ab035b
AbayReushenov <abay.reushenov@gmail.com>
2020-06-22 14:39:49 +0300

    Объединены все уроки в один репозитарий

---
+++ js/lesson25.js
@@ -0,0 +1,19 @@
+'use strict';
+console.log(-55.02);
+debugger;
+console.log(-1);
+debugger;
+console.log(1-1);
+debugger;
+console.log(1);
+debugger;
+console.log(2);
+debugger;
+
+alert("Say Hello!"
+      /* Console =>
+                    -55.02
+            lesson25.js:4 -1
+            lesson25.js:6 0
+            lesson25.js:8 1
+            lesson25.js:10 2 */
