e4756d1a68ee7c73a635e07de16b0b126a99c1a3
AbayReushenov <abay.reushenov@gmail.com>
2020-07-04 13:07:36 +0300

    Lesson 41 Timer

--- Lesson38/Food_dist/js/script.js
+++ Lesson38/Food_dist/js/script.js
@@ -22,7 +22,7 @@
 
 // document.addEventListener("DOMContentLoaded", ready);
 window.addEventListener("DOMContentLoaded", () => {
-    
+    //tabs
     const tabs = document.querySelectorAll(".tabheader__item"), // табы
         tabsContent = document.querySelectorAll(".tabcontent"), // контент к табам
         tabsParent = document.querySelector(".tabheader__items"); //родитель табов, для делегирования
@@ -45,7 +45,7 @@ window.addEventListener("DOMContentLoaded", () => {
             });
         }
 
-        // функция для показа табов
+    // функция для показа табов
 
         function showTabContent(i = 0) {
             // tabsContent[i].style.display = 'block';
@@ -72,5 +72,77 @@ window.addEventListener("DOMContentLoaded", () => {
          }
     });
    
+    // Timer
 
+    const deadline = '2020-07-05';
+    const t = Date.parse(deadline),
+          t1 = new Date(deadline),
+          t2 = new Date(t),
+          s = Date.parse(new Date()),
+          d = new Date(),
+          d1 = d.getTime();
+          console.log(t);  // t = Date.parse(deadline), => 1589155200000
+          console.log(t1);  //t1 = new Date(deadline),  => 2020-05-11T00:00:00.000Z
+          console.log(t2);  //t2 = new Date(t),         => 2020-05-11T00:00:00.000Z
+          console.log(s);  // s = Date.parse(new Date()); => 1593844649000
+          console.log(d);  //  d = new Date();   => 2020-07-04T06:37:29.400Z
+          console.log(d1);  // d1 = d.getTime(); => 1593844649000
+          const xt = Date.parse(deadline) - Date.parse(new Date()) - 3 * 60 * 60 * 1000;
+          console.log( Math.floor(xt / (1000 * 60 * 60) % 24));
+
+
+    function getTimeRemining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()) - 3 * 60 * 60 * 1000, 
+        // скорректируем для учета разницы с UTF на 3 часа
+              days =  Math.floor( t / (1000 * 60 * 60 * 24)),  //целых дней
+              hours = Math.floor( t / (1000 * 60 * 60 ) % 24 ),    // остаток часов от дней
+              minutes = Math.floor( t / (1000 * 60 ) % ( 60) ), // остаток минут от часов
+              seconds = Math.floor( t / (1000 ) % ( 60 ) ); // остаток секунд от минут
+        // console.log(t);
+        // console.log(days);
+        // console.log(hours);
+        // console.log(minuts);
+        // console.log(seconds);
+        return{
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else { 
+            return num;
+            }
+    }
+
+    function setClock(selector, endtime)   {         
+        // const timer = document.querySelector('.timer'),  // <div class="timer">
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();   // инициализация , для решения вопроса с миганием экрана
+            
+        function updateClock() {
+            const t = getTimeRemining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('.timer', deadline);
 });
