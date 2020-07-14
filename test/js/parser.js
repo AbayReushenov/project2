673253c31b32a968004575622d5b40540c6ba6d8
AbayReushenov <abay.reushenov@gmail.com>
2020-07-14 23:04:05 +0300

    Test project: 1. Parser, 2. Fetch, 3. Axios. After lesson 60

--- test/js/parser.js
+++ test/js/parser.js
@@ -1,37 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
     const body = document.querySelector('body');
     let textNodes = [];
-    
-    // function recursy (elementos){
-    //     elementos.childNodes.forEach( node => {
-    //         console.log(node);
-    //         if (elementos.childNodes.lenght > 1) {
-    //             recursy(node); 
-    //         }
-    //     });
-    // }
-
-    // recursy(body);
-
-    // 2- этап
-
-    // function recursy (elementos){
-    //     elementos.childNodes.forEach( node => {
-    //         if (node.nodeName === '#text') {
-    //             return; 
-    //         } else {
-    //             console.log(node);
-    //             recursy(node);
-    //         }
-    //     });
-    // }
-
-    // recursy(body);
-
-    // 3 этап
-
-
-
+ 
     function recursy (elementos){
         elementos.childNodes.forEach( node => {
             // для загаловков H1, H2 etc.
@@ -59,41 +29,13 @@ window.addEventListener("DOMContentLoaded", () => {
     }) 
     .then(response => response.json())
     .then(json => console.log(json));
+}); 
 // : {…}, 1: {…}, 2: {…}, id: 101}
 // 0: {header: "H1", content: "Работа с Jason-server"}
 // 1: {header: "H2", content: "1. Терминал"}
 // 2: {header: "H1", content: "script59ProverkaOtIvana.js"}
 // id: 101
-});  
+ 
 
 
 // https://jsonplaceholder.typicode.com/
-//
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-// .then(response => response.json())
-// .then(json => console.log(json))
-
-
-//     Routes
-// All HTTP methods are supported.
-
-// GET	/posts
-// GET	/posts/1
-// GET	/posts/1/comments
-// GET	/comments?postId=1
-// GET	/posts?userId=1
-// POST	/posts
-// PUT	/posts/1
-// PATCH	/posts/1
-// DELETE	/posts/1
-    // console.log(textNodes); 
-    // ["Работа с Jason-server", "1. Терминал", "script59ProverkaOtIvana.js"]
-
-
-
-
-
-// выделяем а затем
-// console.dir($0)
-// nodeName: "H3"
-// nodeType: 1
--- test/js/script.js
+++ test/js/script.js
@@ -4,7 +4,6 @@
     // .then(data => console.log(data)); 
 
 window.addEventListener("DOMContentLoaded", () => {
-    
     // getResourse будет отвечать за получение данных для карточек
     const getResourse = async (url) => {
         const result = await fetch(url);
@@ -18,82 +17,27 @@ window.addEventListener("DOMContentLoaded", () => {
     getResourse("http://localhost:3000/menu")
         .then(data => plusCard(data));
 
-    function plusCard(data) {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            const bit = document.createElement('div');
+function plusCard(data) {
+    data.forEach(({img, altimg, title, descr, price}) => {
+        const bit = document.createElement('div');
 
-            price *= 27;
+        price *= 27;
 
-            bit.classList.add('menu__item');
+        bit.classList.add('menu__item');
 
-            bit.innerHTML = `
-                <img src=${img} alt=${altimg}>
-                <h3 class="menu__item-subtitle">${title}</h3>
-                <div class="menu__item-descr">${descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${price}</span> грн/день</div>
-                </div>
-            `;
+        bit.innerHTML = `
+            <img src=${img} alt=${altimg}>
+            <h3 class="menu__item-subtitle">${title}</h3>
+            <div class="menu__item-descr">${descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${price}</span> грн/день</div>
+            </div>
+        `;
 
-            document.querySelector('.menu').append(bit);
+        document.querySelector('.menu').append(bit);
         });
     }
-
-    // Forms Lesson 53
-    // const formS = document.querySelectorAll('form');
-
-    // const mess = {
-    //     loading: 'Загрузка',
-    //     success: 'Спасибо! Мы с Вами скоро свяжемся.',
-    //     failure: 'Что-то пошло не так ...',
-    // };
-
-    // formS.forEach(id => {
-    //     bindPostData(id);
-    // });
-
-    // // postData будет отвечать за posting данных
-    // const postData = async (url, data) => {
-    //     const result = await fetch(url, {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-type': 'application/json'
-    //         },
-    //         body: data
-    //     });
-
-    //     return await result.json();
-    // };
-
-    // // данная функция будетотвечать за привязку данных
-    // function bindPostData(forma) {
-    //     forma.addEventListener("submit", (e) => {
-    //         e.preventDefault(); // в AJAX обязательно
-
-    //         const statusnyimessage = document.createElement('img');
-    //         statusnyimessage.src = mess.loading;
-    //         statusnyimessage.style.cssText = `
-    //             display: block;
-    //             margin: 0 auto; 
-    //         `;
-    //         forma.insertAdjacentElement('afterend', statusnyimessage);
-
-    //         const formData = new FormData(forma);
-
-    //         const json = JSON.stringify(Object.fromEntries(formData.entries()));
-
-    //         postData('http://localhost:3000/requests', json )
-    //         .then(data => {
-    //             console.log(data);
-    //             showThanksModalSpecial(mess.success);
-    //             statusnyimessage.remove();
-    //         })
-    //         .catch(() => {
-    //             showThanksModalSpecial(mess.failure);
-    //         })
-    //         .finally(() => forma.reset());
-    //         });
-    // }
 });
+
