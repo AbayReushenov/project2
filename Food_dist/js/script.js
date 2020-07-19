b5213734f77e82671a624d9af7af895b92cd1354
AbayReushenov <abay.reushenov@gmail.com>
2020-07-19 21:46:31 +0300

    67. Создаем калькулятор на сайте, часть 2

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -416,13 +416,44 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    // Calc
+    // Calc lesson 66, 67
 
     const kilocalor = document.querySelector('.calculating__result span');
     // <div class="calculating__result">
     // <span>2700</span> ккал
     // </div>
-    let sex = 'female', height, weight, age, ratio = 1.375;
+     let sex, height, weight, age, ratio = 1.375;
+
+     if (localStorage.getItem('sex')) {
+         sex = localStorage.getItem('sex');
+     } else {
+            sex = 'female';
+            localStorage.setItem('sex', 'female');
+     }
+
+     if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
+           ratio = '1.375';
+           localStorage.setItem('ratio', '1.375');
+    }
+
+    function initLocalSettings(selecot, activoClass) {
+        const elements = document.querySelectorAll(selecot);
+
+        elements.forEach(eLem => {
+            eLem.classList.remove(activoClass);
+            if (eLem.getAttribute('id') === localStorage.getItem('sex')) {
+                eLem.classList.add(activoClass);
+            }
+            if (eLem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                    eLem.classList.add(activoClass);   
+            }
+        });
+    }
+
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
 
     function calcTotal() {
         if (!sex || !height || !weight || !age || !ratio) {
@@ -439,8 +470,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
     calcTotal();
 
-    function getStatInfo(selectorRoditel, classFlagAktiven) {
-        const elementsCalc = document.querySelectorAll(`${selectorRoditel} div`);
+    function getStatInfo(selector, classFlagAktiven) {
+        const elementsCalc = document.querySelectorAll(selector);
         //подучим все дивы
 
 
@@ -452,8 +483,10 @@ window.addEventListener("DOMContentLoaded", () => {
             itemDiv.addEventListener('click', (event) => {
                 if (event.target.getAttribute('data-ratio')) {
                     ratio = +event.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', +event.target.getAttribute('data-ratio'));
                 } else {
                     sex = event.target.getAttribute('id');
+                    localStorage.setItem('sex', event.target.getAttribute('id'));
                 }
     
                 elementsCalc.forEach(intem => {
@@ -486,13 +519,23 @@ window.addEventListener("DOMContentLoaded", () => {
         //     });
     }
 
-    getStatInfo('#gender', 'calculating__choose-item_active');
-    getStatInfo('.calculating__choose_big', 'calculating__choose-item_active');
+    getStatInfo('#gender div', 'calculating__choose-item_active');
+    getStatInfo('.calculating__choose_big div', 'calculating__choose-item_active');
 
     function getInputOrDinamicInfo(selectorID) {
         const nashInput = document.querySelector(selectorID);
+// добавим проверку на ввод корректных данных, с обратной связью для "user"
+        nashInput.addEventListener('input', () => { // событие 'input' состоялось
+// добавим проверку на ввод корректных данных, с обратной связью для "user"
 
-        nashInput.addEventListener('input', (eee) => { // событие 'input' состоялось
+             if (nashInput.value.match(/\D/g)) { // вводится не число
+                nashInput.style.border = '1px solid red';
+                nashInput.classList.add('blink');
+
+             } else {
+                nashInput.style.border = 'none';
+                nashInput.classList.remove('blink');
+             }
             switch(nashInput.getAttribute('id')) { //  in this div there is 'id'
             // if there is === 'true'
                 case 'height' :
