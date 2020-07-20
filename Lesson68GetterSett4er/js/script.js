231b1b4d7cca8b1fcb918740c0dad46ebed155ff
AbayReushenov <abay.reushenov@gmail.com>
2020-07-20 21:17:27 +0300

    Lesson 69 Икапсуляция get and set #properties

--- Lesson68GetterSett4er/js/script.js
+++ Lesson68GetterSett4er/js/script.js
@@ -1,37 +1,59 @@
 'use strict';
 
-const chel = {
-    name: "Evgeniy",
-    age: 25,
-    bisnessIncome: 100,
-    get chelBisiness() {
-        return this.bisnessIncome;
-    },
-    wife: " NAtasha",
-    set chelBisiness(newInfo) {
-        this.bisnessIncome = newInfo;
+class UserStandart {
+    constructor(xName, xAge) {
+        this.name = xName;
+    // this.age = xAge;
+        this._userAge = xAge;
     }
-};
 
-console.log( chel.bisnessIncome);
-console.log( chel.chelBisiness);
-console.log('---');
+    //вне конструктора!!!
+    #surname = 'Reushenov';
 
-console.log( chel.bisnessIncome = 500);
-console.log( chel.bisnessIncome);
-console.log( chel.chelBisiness);
-console.log('---');
+    functiuonSay() {
+        console.log(`User name is: ${this.name} ${this.#surname}, age is ${this._userAge}`);
+    }
 
-console.log( chel.chelBisiness = 200);
-console.log( chel.bisnessIncome);
-console.log( chel.chelBisiness);
-console.log('---');
+    functiuonSay3 = () => {
+        console.log(`User name is: ${this.name} ${this.#surname}, age is ${this._userAge}`);
+    }
 
-chel.bisnessIncome = 0;
-console.log( chel.bisnessIncome);
-console.log( chel.chelBisiness);
-console.log('---');
+    get age() {
+        return this._userAge;
+    }
 
-chel.chelBisiness = 2000;
-console.log( chel.bisnessIncome);
-console.log( chel.chelBisiness);
+    set age(xAge) {
+        if (typeof xAge === 'number' && xAge > 0 && xAge < 110) {
+            this._userAge = xAge;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    }
+
+    get surnameGet() {
+        return this.#surname;
+    }
+
+    set surnameSet(surname) {
+        if (typeof surname === 'string' && surname.length < 50) {
+            this.#surname = surname;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    }
+}
+
+
+const abay = new UserStandart('Abay', 53);
+
+abay.functiuonSay();
+abay.functiuonSay3();
+console.log(abay.surname);
+// console.log(abay.#surname);
+console.log(abay.surnameGet);
+abay.surnameSet = "Fedorovitch";
+console.log(abay.surnameGet);
+abay.functiuonSay();
+abay.functiuonSay3();
+
+
